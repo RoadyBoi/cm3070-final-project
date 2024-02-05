5aa44ef0cd10b85ba0a9ece5061a2d4a51806ecd
RoadyBoi <roaidmustafa999@gmail.com>
2024-02-05 19:33:25 +0500

    initial game logic

--- lib/controllers/game_copy.dart
+++ lib/controllers/game_copy.dart
@@ -1,63 +1,100 @@
 import 'dart:math';
 import 'dart:io';
+import 'dart:convert';
 
 void main() {
-  int rnd = getRandomNumber();
-  String firstLetter = getFirstLetter();
-  List<String> gameArray = generateGameArray(rnd, firstLetter);
-  print(gameArray); // Print initial gameArray
-  fillGameArray(gameArray, rnd);
-  // print(gameArray); // Print gameArray after filling
-  print(userInput(rnd, gameArray));
+  LaneGame.populateIndexedWordMap();
 }
 
-String getFirstLetter() {
-  Random random = Random();
-  int randomNumber = random.nextInt(26);
-  List<String> outputArray =
-      List.generate(26, (index) => String.fromCharCode(index + 65));
-  return outputArray[randomNumber];
-}
+class LaneGame {
+  static List<String> currentGameArray = [];
+  static int currentRandomNumber = 0;
 
-int getRandomNumber() {
-  Random random = Random();
-  int min = 3;
-  int max = 9;
-  int r = min + random.nextInt(max - min);
-  return r;
-}
+  static Map indexedWordMap = {};
+  static List<String> playedWords = [];
 
-List<String> generateGameArray(int randomNumber, String firstLetter) {
-  List<String> emptyStringArray = List<String>.filled(randomNumber, '');
-  emptyStringArray[0] = firstLetter;
-  return emptyStringArray;
-}
-
-List<String> userInput(int rndNum, List<String> gArray) {
-  String? input = stdin.readLineSync();
-  // Check if input is not empty
-  if (input != null && input.isNotEmpty) {
-    return input
-        .substring(0, input.length <= rndNum ? input.length : rndNum)
-        .split('')
-        .map((x) => x.toUpperCase())
-        .toList();
-  } else {
-    return [];
+  static String getFirstLetter() {
+    Random random = Random();
+    int randomNumber = random.nextInt(26);
+    List<String> letterList =
+        List.generate(26, (index) => String.fromCharCode(index + 97));
+    return letterList[randomNumber];
   }
-}
 
-List<String> fillGameArray(List<String> gArray, int rndNum) {
-  for (int i = 1; i < rndNum; i++) {
-    if (gArray[i] == '') {
-      List<String> userInputList = userInput(1, gArray);
-      if (userInputList.isNotEmpty) {
-        gArray[i] = userInputList[0];
-        print(gArray); // Print gameArray after each modification
-      } else {
-        i--; // Decrement i to reprocess the same index in case of empty input
-      }
+  static void calculateRandomNumber() {
+    currentRandomNumber = 3 + Random().nextInt(9 - 3);
+  }
+
+  static List<String> generateFirstGameArray() {
+    List<String> emptyStringArray =
+        List<String>.filled(currentRandomNumber, '');
+    emptyStringArray[0] = getFirstLetter();
+    return emptyStringArray;
+  }
+
+  static String? userInput() {
+    String? input = stdin.readLineSync();
+    // Check if input is not empty
+    if (input != null && input.isNotEmpty) {
+      return input.trim().toLowerCase()[0];
+    } else {
+      return null;
     }
   }
-  return gArray;
+
+  static List<String> fillGameArray(List<String> gArray, int gameWordLength) {
+    for (int i = 1; i < gameWordLength; i++) {
+      print(i);
+      if (gArray[i] == '') {
+        String? userInputLetter = userInput();
+        if (userInputLetter != null && userInputLetter.isNotEmpty) {
+          gArray[i] = userInputLetter;
+          print(gArray); // Print gameArray after each modification
+        } else {
+          i--; // Decrement i to reprocess the same index in case of empty input
+        }
+      }
+    }
+    return gArray;
+  }
+
+  // one-time use only (command-line dart)
+  static void generateIndexedJson() {
+    File jsonSourceFile = File("../../assets/words_dictionary.json");
+    Map wordMap = json.decode(jsonSourceFile.readAsStringSync());
+    Map indexedWordMap = {};
+    String currentWord;
+
+    wordMap.entries.forEach((entry) {
+      // word in json list
+      currentWord = entry.key.trim().toLowerCase();
+      // if no first letter entry in result json map, create entry with key = word and value = empty list
+      if (!indexedWordMap.containsKey(currentWord[0])) {
+        indexedWordMap[currentWord[0]] = [];
+      }
+      // filter word in word list by length and add to result json map inside the key index list
+      if (currentWord.length > 2 && currentWord.length < 9) {
+        indexedWordMap[currentWord[0]].add(currentWord);
+      }
+    });
+
+    // save the indexed word list
+    File jsonResultFile = File("../../assets/words_dictionary_index.json");
+    jsonResultFile.writeAsStringSync(json.encode(indexedWordMap));
+  }
+
+  // To be called on app start or game start for refreshing of list
+  static void populateIndexedWordMap() {
+    indexedWordMap = json.decode(
+        File("../../assets/words_dictionary_index.json").readAsStringSync());
+  }
+
+  static bool validateWord(List<String> wordArray) {
+    // first letter to be used as index
+    String firstLetter = wordArray[0];
+    String currentPlayedWord = wordArray.join().trim().toLowerCase();
+
+    return (!playedWords.contains(currentPlayedWord)) &&
+        (indexedWordMap[firstLetter]?.contains(currentPlayedWord) ?? false);
+  }
 }
